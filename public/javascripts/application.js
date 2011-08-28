e98fde7ebacde39a58877115940c173a4c62466f
Logan Koester <logan@logankoester.com>
2011-08-28 00:46:41 -0400

    Add basic jquery ui interface

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,23 @@
 google.load("jquery", "1.6.2");
+google.load("jqueryui", "1.8.16");
 
 function init() {
   $(document).ready(function(){
+    
+    // Toggle display of filters and activity
+    $('#filters-accordion').accordion({
+        collapsible: true
+    });
+    $('#activity-accordion').accordion({
+        collapsible: true
+    });
+
+    // Organize filters with a tabbed interface
+    $('#filters').tabs();
+
+    // Filters can be toggled on and off
+    $('.filter').button();
+
   });
 }
 
