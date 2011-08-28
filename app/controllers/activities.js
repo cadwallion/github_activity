07717cefadc753f46b5c4dd16b7ea924712d0d5c
Logan Koester <logan@logankoester.com>
2011-08-28 16:12:42 -0400

    Render results as html

--- app/controllers/activities.js
+++ app/controllers/activities.js
@@ -4,8 +4,11 @@ module.exports = {
 
     projects = JSON.parse(req.body.projects);
     Github.getCommitsByRepo(projects, function(data) { 
-      res.contentType('application/json');
-      res.send(data);
+      console.log(data);
+      res.render('activities/results.jade', {
+          layout: false,
+          data: data
+      });
     });
   }
 }
