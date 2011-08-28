3f5217f66d505d9e187d291e294d47d99f9f3279
Logan Koester <logan@logankoester.com>
2011-08-28 04:48:49 -0400

    Chunk buttons into sets

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,35 @@
 google.load("jquery", "1.6.2");
 google.load("jqueryui", "1.8.16");
 
+function arrayChunk(array, size) {
+    var start = 0, result = [], chunk = [];
+    while((chunk = array.slice(start, start += size)).length) {
+        result.push(chunk);
+    }
+    return result;
+}
+
 function load_trending_repos(){
-  $.getJSON('/trending_repos.json', function(repos){
-    console.log('repos loaded');
-    $.each(repos, function(k,repo){
-      // TODO Create the button
+  $.getJSON('/trending_repos.json', function(result){
+    var buttons_per_set = 5;
+    var repo_chunks = arrayChunk(result, buttons_per_set);
+
+    $.each(repo_chunks, function(k, repos){
+      $('#featured-repos-filters').append('<div class=".set"></div>');
+      var current_set = $('#featured-repos-filters :last');
+
+      $.each(repos, function(k,repo){
+        // Generate the markup
+        var repo_slug = repo.replace(/\/|\./, '_');
+        var input = '<input class="filter" id="' + repo_slug + '" name="' + repo_slug + '" type="checkbox" checked="true" />';
+        var label = '<label for="' + repo_slug + '">' + repo + '</label>';
+
+        current_set.append(input);
+        current_set.append(label);
+      });
+      current_set.buttonset();
     });
+
   });
 }
 
