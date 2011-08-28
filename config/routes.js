be592db470814679447f3a906a7979a054c03a76
Andrew Nordman <cadwallion@gmail.com>
2011-08-27 22:07:34 -0500

    Added authRequired middleware to routes.js

--- config/routes.js
+++ config/routes.js
@@ -4,5 +4,17 @@ module.exports.loadRoutes = function (app) {
   // alternatively, user auth middleware on a route
   //app.get('/another/url', authorizeRequired, Server.controllers.resource.action);
 
+  /* 
+     Middleware to enforce authorization. Example:
+       app.get('/test/url', [authRequired], Server.controllers.test.url_action)
+   */
+  function authRequired(req, res, next) {
+    if (req.loggedIn()) {
+      next();
+    } else {
+      res.redirect('/auth/github');
+    }
+  }
+
   app.resource(Server.controllers.index);
 }
