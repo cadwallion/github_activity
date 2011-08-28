15852766545616d20211b7f072263004f462f8a9
Andrew Nordman <cadwallion@gmail.com>
2011-08-27 20:32:18 -0500

    added example of route-based control middleware

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 module.exports.loadRoutes = function (app) {
   //app.resource('users', Server.controllers.user);
   //app.get('/some/url', Server.controllers.resource.action);
+  //app.get('/another/url', authorizeRequired, Server.controllers.resource.action);
 
   app.resource(Server.controllers.test);
 }
