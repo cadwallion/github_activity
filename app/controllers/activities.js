3d25a681dbe15edfc295c3bbcf906883b4891540
Logan Koester <logan@logankoester.com>
2011-08-28 16:44:41 -0400

    Good start on parsing

--- app/controllers/activities.js
+++ app/controllers/activities.js
@@ -2,12 +2,25 @@ module.exports = {
   index: function (req, res) {
     var Github = require('../models/github');
 
-    projects = JSON.parse(req.body.projects);
-    Github.getCommitsByRepo(projects, function(data) { 
+    filters = JSON.parse(req.body.projects);
+    Github.getCommitsByRepo(filters, function(data) { 
       console.log(data);
+
+      var projects = []
+      // Parse the commit data into a format for views
+      for (key in data) {
+        var project = new Object();
+        project.owner = key.split('/')[0];
+        project.name = key.split('/')[1];
+        project.commits = data[key];
+        projects.push(project);
+      };
+
+
+      console.log(projects);
       res.render('activities/results.jade', {
           layout: false,
-          data: data
+          projects: projects
       });
     });
   }
