5e39a79a88d464ade48c8d936b9d23cdab419644
Andrew Nordman <cadwallion@gmail.com>
2011-08-28 14:55:17 -0500

    remove cruft that breaks activities

--- app/controllers/activities.js
+++ app/controllers/activities.js
@@ -2,8 +2,6 @@ module.exports = {
   index: function (req, res) {
     var Github = require('../models/github');
 
-    res.expose(res.user, 'express.user');
-    res.expose(req.loggedIn, 'express.loggedIn');
     projects = JSON.parse(req.body.projects);
     Github.getCommitsByRepo(projects, function(data) { 
       res.contentType('application/json');
